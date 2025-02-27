d034dada52463d0d0265f24dda4e7331ae98e4cd
Weversonmn <weversonmn@gmail.com>
2025-02-27 10:27:40 -0300

    Atualizando

---
+++ ControleFluxo/src/ExemploFor2.java
@@ -0,0 +1,10 @@
+public class ExemploFor2 {
+    public static void main(String[] args) {
+        String alunos [] = {"Pedro", "Tiago","João", "Julia"};
+        
+        for( int x=0; x < alunos.length; x++){
+
+            System.out.println("O Aluno do Indice x= " + x + " é " + alunos[x]);
+        }
+    }
+}
