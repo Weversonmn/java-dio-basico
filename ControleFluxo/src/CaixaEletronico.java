db68cf317e8f5e575733d2687994fb535bdf02db
Weversonmn <weversonmn@gmail.com>
2025-02-27 10:37:15 -0300

    alteração codigo

--- ControleFluxo/src/CaixaEletronico.java
+++ ControleFluxo/src/CaixaEletronico.java
@@ -1,6 +1,6 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
-        double saldo = 25.0;
+        double saldo = 30.0;
         double valorSolicitado = 14.0;
 
         if(valorSolicitado < saldo){
